84e8f715e668ce1ce308a8dbf45458fc6d809bfd
Kristian Kemalov <114097437+KristianKemalov@users.noreply.github.com>
2023-03-03 19:26:22 +0200

    Add exercises

---
+++ Inheritance/Stack/StackOfStrings.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.List;
+
+public class StackOfStrings {
+    private final List<String> data;
+
+    public StackOfStrings() {
+        this.data = new ArrayList<>();
+    }
+
+    public void push(String item) {
+        this.data.add(item);
+
+    }
+
+    public String peek() {
+        if (this.isEmpty()) {
+            throw new EmptyStackException();
+
+        }
+        int index = this.size() - 1;
+        return this.data.get(index);
+
+    }
+
+    public int size() {
+        return this.data.size();
+
+    }
+
+    public String pop() {
+        if (this.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        int index = this.size() - 1;
+
+        return this.data.remove(index);
+
+
+    }
+
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
+}
